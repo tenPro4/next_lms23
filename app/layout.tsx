651b2a8c3fb0af88bd6412607be38b4c2ce94c01
wengshang <wengshang.hoo@sophicautomation.com>
2023-10-30 10:43:00 +0800

    init

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
+import { ToastProvider } from '@/components/providers/toaster-provider'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { ConfettiProvider } from '@/components/providers/confetti-provider'
+import {ClerkProvider} from '@clerk/nextjs'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,8 +18,14 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <ConfettiProvider />
+          <ToastProvider />
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }
